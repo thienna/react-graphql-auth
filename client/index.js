5f26d14a227e738b856f149658b9b026aa22c39a
Thien Nguyen <mikenguyen@Mikes-MacBook-Air.local>
2021-05-29 02:31:17 +0700

    setup client side

--- client/index.js
+++ client/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+})
 
 const Root = () => {
   return (
-    <div>
+    <ApolloProvider client={client}>
       Auth Starter
-    </div>
+    </ApolloProvider>
   );
 };
 
--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,12 +1,14 @@
 const graphql = require('graphql')
 const {
     GraphQLObjectType,
+    GraphQLID,
     GraphQLString
 } = graphql
 
 const UserType = new GraphQLObjectType({
     name: 'UserType',
     fields: {
+        id: { type: GraphQLID },
         email: {
             type: GraphQLString
         }
