64eb43e7f3004e1f3c1a8bebc2a4473ae1f5e525
Thien Nguyen <mikenguyen@Mikes-MacBook-Air.local>
2021-05-29 14:50:08 +0700

    component structure

--- client/index.js
+++ client/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
+import { Router, hashHistory, Route, IndexRoute } from 'react-router'
+
+import App from './components/App'
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
@@ -10,7 +13,10 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      Auth Starter
+      <Router history={hashHistory}>
+        <Route to="/" component={App}></Route>
+        <IndexRoute />
+      </Router>
     </ApolloProvider>
   );
 };
