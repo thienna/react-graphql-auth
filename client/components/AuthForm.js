e850865b18a2382db3e2ed7c26701b2d69d606c9
Thien Nguyen <mikenguyen@Mikes-MacBook-Air.local>
2021-06-03 01:44:09 +0700

    handle login with error

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -9,18 +9,34 @@ class AuthForm extends Component {
         }
     }
 
+    onSubmit(event) {
+        event.preventDefault()
+
+        this.props.onFormSubmit(this.state)
+    }
+
     render() {
         return (
             <div className="row">
-                <form className="col s4 m6">
+                <form className="col s4 m6" onSubmit={this.onSubmit.bind(this)}>
                     <div class="input-field">
                         <label>Email:</label>
-                        <input value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} />
+                        <input 
+                        placeholder="Email"
+                        value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} />
                     </div>
                     <div class="input-field">
                         <label>Password:</label>
-                        <input value={this.state.password} onChange={(event) => this.setState({ password: event.target.value })} />
+                        <input 
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.password} onChange={(event) => this.setState({ password: event.target.value })} />
                     </div>
+
+                    <div className="errors">
+                        {this.props.errors.map(error => <div key={error}>{error}</div>)}
+                    </div>
+
                     <button className="btn">Submit</button>
                 </form>
             </div>
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
 
 import AuthForm from './AuthForm'
+import mutationLogin from '../mutations/Login'
+import QueryCurrentUser from '../queries/CurrentUser'
 
 class LoginForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            errors: []
+        }
+    }
+
+    onSubmit({ email, password }) {
+        this.props.mutate({
+            variables: { email, password },
+            refetchQueries: [{query: QueryCurrentUser}]
+        }).catch(err => { 
+            const errors = err.graphQLErrors.map(item => item.message)
+            this.setState({ errors })
+         })
+    }
+
     render() {
         return (
             <div>
                 <h3>Login</h3>
-                <AuthForm />
+                <AuthForm onFormSubmit={this.onSubmit.bind(this)} errors={this.state.errors} />
             </div>
         )
     }
 }
 
-export default LoginForm
+export default graphql(mutationLogin)(LoginForm)
